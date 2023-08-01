3f2062ea4f7e5c50b279ab1f123f2b822a4a8921
Kevin Nocon <kevin.nocon@uni-siegen.de>
2023-08-01 21:52:16 +0200

    MusicBot Start

---
+++ commands/fun/play.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('Play a spotify song!'),
+	async execute(interaction) {
+		await interaction.reply('To be done!');
+	},
+};
