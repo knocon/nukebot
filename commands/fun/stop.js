6b2c6a8cd55e4daed91293cbb510ad539d5295e1
Kevin Nocon <kevin.nocon@uni-siegen.de>
2023-08-01 22:36:03 +0200

    MusicBot

---
+++ commands/fun/stop.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('stop')
+		.setDescription('Stops current song!'),
+	async execute(interaction) {
+		await interaction.reply('To be done!');
+	},
+};
