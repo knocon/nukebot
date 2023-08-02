517571ce811360ef9b87ab7c996fe3241a2acecb
knocon <knocon96@gmail.com>
2023-08-02 19:20:51 +0200

    DynamicChannels improved

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,7 +1,7 @@
 const { Events } = require('discord.js');
 
 const targetChannelId = '1135601577533648906';
-const clonedChannels = new Map();
+const dynamicChannels = new Map();
 
 module.exports = {
 	name: Events.VoiceStateUpdate,
@@ -9,10 +9,14 @@ module.exports = {
 	async execute(oldState, newState) {
 		if (newState.channelId === targetChannelId) {
 			console.log('User joined target channel to clone.');
-
-			const newChannel = await newState.channel.clone();
-			clonedChannels.set(newChannel.id, newChannel);
-
+			const newChannel = await newState.guild.channels.create({
+				name: '☢️',
+				type: 2,
+				parent: '1134547323624624259',
+				userLimit: 1,
+			});
+			dynamicChannels.set(newChannel.id, newChannel);
+			
 			const member = newState.member;
 			if (member) {
 				await member.voice.setChannel(newChannel);
@@ -22,5 +26,5 @@ module.exports = {
 
 		}
 	},
-	clonedChannels,
+	dynamicChannels,
 };
--- events/channelDestroy.js
+++ events/channelDestroy.js
@@ -1,11 +1,11 @@
 const { Events } = require('discord.js');
-const { clonedChannels } = require('../events/channelCreate');
+const { dynamicChannels } = require('../events/channelCreate');
 
 module.exports = {
 	name: Events.VoiceStateUpdate,
 
 	async execute(oldState) {
-		if (clonedChannels.has(oldState.channelId) && oldState.channel.members.size === 0) {
+		if (dynamicChannels.has(oldState.channelId) && oldState.channel.members.size === 0) {
 			oldState.channel.delete();
 		}
 	},
