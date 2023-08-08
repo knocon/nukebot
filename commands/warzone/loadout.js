1a6fdd479e1854e34e4a4ac8440837b2b0320945
knocon <knocon96@gmail.com>
2023-08-09 01:17:33 +0200

    update loadout.js

--- commands/warzone/loadout.js
+++ commands/warzone/loadout.js
@@ -1,6 +1,29 @@
 class Weapon {
-    constructor(name, type) {
+    constructor(
+        name,
+        type,
+        muzzle,
+        barrel,
+        laser,
+        optic,
+        stock,
+        underbarrel,
+        ammunition,
+        magazine,
+        reargrip,
+        triggeraction,
+    ) {
         this.name = name;
         this.type = type;
+        this.muzzle = muzzle;
+        this.barrel = barrel;
+        this.laser = laser;
+        this.optic = optic;
+        this.stock = stock;
+        this.underbarrel = underbarrel;
+        this.ammunition = ammunition;
+        this.magazine = magazine;
+        this.reargrip = reargrip;
+        this.triggeraction = triggeraction;
     }
 }
