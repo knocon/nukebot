cb45ca353aa092cd771b8bb1205f42ece438a177
knocon <knocon96@gmail.com>
2023-08-09 05:41:26 +0200

    delete 1 file, update 1 file and create 1 file

--- commands/warzone/meta.js
+++ commands/warzone/meta.js
@@ -1,12 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { season5 } = require('./season5.js');
+
+const formattedLoadouts = season5
+    .map((loadout, index) => {
+        return `
+      // META WEAPON #${index + 1}
+      WEAPON: '${loadout.weapon} [${loadout.type}]'
+      '${loadout.attachments.first.type}': '${loadout.attachments.first.name}' 
+      '${loadout.attachments.second.type}': '${loadout.attachments.second.name}'
+      '${loadout.attachments.third.type}': '${loadout.attachments.third.name}'
+      '${loadout.attachments.fourth.type}': '${loadout.attachments.fourth.name}'
+      '${loadout.attachments.fifth.type}': '${loadout.attachments.fifth.name}'
+    `;
+    })
+    .join('\n');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('meta')
         .setDescription('Provides current meta loadout for warzone season 5.'),
     async execute(interaction) {
-        // interaction.user is the object representing the User who ran the command
-        // interaction.member is the GuildMember object, which represents the user in the specific guild
-        await interaction.reply('not implemented yet!');
+        await interaction.reply(`
+        \`\`\`js
+        CURRENT META WEAPONS FOR SEASON 5 WARZONE
+        
+        ${formattedLoadouts}
+        \`\`\`
+      `);
     },
 };
---
+++ commands/warzone/season5.js
@@ -0,0 +1,30 @@
+const season5 = [
+    {
+        weapon: 'CRONEN SQUALL',
+        type: 'Battlerifle',
+        attachments: {
+            first: {
+                type: 'muzzle',
+                name: 'Sakin Tread-40 ⬆️+0.39 ➡️+0.26',
+            },
+            second: {
+                type: 'barrel',
+                name: 'HR6.8 Barrel',
+            },
+            third: {
+                type: 'optic',
+                optic: 'Aim OP-V4',
+            },
+            fourth: {
+                type: 'underbarrel',
+                name: 'FTAC Ripper 56',
+            },
+            fifth: {
+                type: 'magazine',
+                name: '50 Round Drum',
+            },
+        },
+    },
+];
+
+module.exports = { season5 };
